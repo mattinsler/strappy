dc43fadff8fa5ff3921bcfadee9d44b16912d790
mattinsler <matt.insler@gmail.com>
2011-06-01 09:19:35 -0400

    added extend

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 exports.Provider = require('./lib/provider');
 exports.Injector = require('./lib/injector');
 exports.bootstrap = require('./lib/strappy');
+exports.extend = require('./lib/extend');
